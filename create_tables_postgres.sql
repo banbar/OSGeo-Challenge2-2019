f9536a82d632cfcee8ce55ab6e7bc4f4ea66ba65
Berk Anbaroğlu <banbar@hacettepe.edu.tr>
2020-02-16 16:33:29 +0300

    Postgres - SQL code to create tables

---
+++ create_tables_postgres.sql
@@ -0,0 +1,30 @@
+﻿create table tree_type
+(
+  name_en character varying(30),
+  name_tr character varying(30),
+  CONSTRAINT tree_types_pkey PRIMARY KEY (name_en)
+)
+
+insert into tree_type(name_en, name_tr) values
+('oak', 'meşe'),
+('pine', 'çam') 
+
+create extension postgis
+
+CREATE TABLE trees
+(
+  t_type character varying(30), 
+  height real,
+  id serial,
+  loc geometry('point', 4326),
+  observation_time timestamp,
+  CONSTRAINT trees_pkey PRIMARY KEY (id)
+)
+
+create table regions
+(
+  region_id serial,
+  responsible character varying(30),
+  region_geom geometry('polygon', 4326),
+  CONSTRAINT regions_pkey PRIMARY KEY (region_id)
+)
